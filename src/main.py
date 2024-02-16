eb4758d56b120b643057e21e21cb8f4457b29c10
Timo Nössler <timo.nowag@icloud.com>
2024-02-16 15:51:13 +0100

    Added Boxplot and copied code to explore the data to Jupyter Notebook

--- src/main.py
+++ src/main.py
@@ -11,3 +11,24 @@ print(df.dtypes)
 df_grouped = df.groupby('Quality').mean()
 print(df_grouped)
 
+df.boxplot(column=['Size', 
+                   'Weight', 
+                   'Sweetness', 
+                   'Crunchiness', 
+                   'Juiciness', 
+                   'Ripeness', 
+                   'Acidity'], 
+           by='Quality', 
+           layout=(1, 7), 
+           figsize=(15, 5))
+
+plt.subplots_adjust(left=0.1,
+                    bottom=0.1, 
+                    right=0.9, 
+                    top=0.9, 
+                    wspace=0.4, 
+                    hspace=0.5)
+
+plt.suptitle('Boxplot of apple quality attributes')
+
+plt.show()
